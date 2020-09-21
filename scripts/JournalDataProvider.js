8e402f12291bf2f65f48278cd1e92e87c0b53d54
Audrey Thomasson <audrey@macbook-pro.lan>
2020-09-21 16:18:36 -0500

    moods updated and working

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -19,7 +19,7 @@ let journal = []
 
 
 export const getEntries = () => {
-    return fetch("http://localhost:8088/entries?_expand=moods") // Fetch from the API
+    return fetch("http://localhost:8088/entries?_expand=mood") // Fetch from the API
     .then(response => response.json())
     .then(
         parsedEntries => {
@@ -41,6 +41,20 @@ export const useEntries = () => {
 export const saveEntry = tacoEntryObj => {
     return fetch("http://localhost:8088/entries", {
         method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(tacoEntryObj)
+    })
+    .then(() => {
+        getEntries()
+    })
+    .then(dispatchStateChangeEvent)
+}
+
+export const deleteEntry = tacoEntryObj => {
+    return fetch("http://localhost:8088/entries", {
+        method: "DELETE",
         headers: {
             "Content-Type": "application/json"
         },
--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -1,4 +1,14 @@
-/*
+import { deleteEntry } from "./JournalDataProvider.js"
+
+const contentTarget = document.querySelector(".entryList")
+
+contentTarget.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "deleteEntry") {
+        removeThisEntry = clickEvent.target.value
+        deleteEntry(removeThisEntry)
+    }
+})
+    /*
  *  Purpose: To render a single journal entry as an
  *           HTML representation of the data
  */
@@ -9,7 +19,8 @@ export const JournalEntryHTML = (entry) => {
         <div> Date: ${entry.date}</div>
         <div>Concept: ${entry.concept}</div>
         <div>Entry: ${entry.entry}</div>
-        <div>Mood:  ${entry.moods.label}</div> 
+        <div>Mood:  ${entry.mood.label}</div>
+        <button type="button" id="deleteEntry" value=${entry.id}>Delete</button> 
         <br>
     </section>
 
